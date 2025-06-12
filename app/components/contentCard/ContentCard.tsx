d14baa962a4f53a266421b7a27e87b86c17f912d
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2025-06-12 10:38:42 +1000

    feat: create content card component for the content

---
+++ app/components/contentCard/ContentCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CategoryCard = () => {
+  return (
+    <li>
+    <article>
+      <h3>Item Fullname 1</h3>
+      <p>Summary text for item 1.</p>
+      <p><strong>Category:</strong> Category Name 1</p>
+      <p><strong>Type:</strong> Content Type 1</p>
+      <p><strong>Tags:</strong> Tag1, Tag2, Tag3</p>
+    </article>
+  </li>
+  )
+}
+
+export default CategoryCard
