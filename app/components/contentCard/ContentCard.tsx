b8d5e3fdf91f0c0ed2bae346a238a6cfeed1eb16
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2025-06-12 15:25:42 +1000

    feat: implement modal in the contentlist component

--- app/components/contentCard/ContentCard.tsx
+++ app/components/contentCard/ContentCard.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import Image from 'next/image';
 
-const ContentCard = ({ item }) => {
+const ContentCard = ({ item, onClick }) => {
   const { fullname, imageurl, summarytext } = item;
 
   return (
-    <li>
-      <a href={item.url} target="_blank" rel="noopener noreferrer">
-        {imageurl ? (
-          <Image src={imageurl} alt={fullname} width={300} height={180} />
-        ) : (
-          ''
-        )}
-        <h3>{fullname}</h3>
-        <p>
-          {summarytext.slice(0, 180)}
-          {summarytext.length > 180 && '...'}
-        </p>
-      </a>
+    <li onClick={() => onClick(item)}>
+      {imageurl ? (
+        <Image src={imageurl} alt={fullname} width={300} height={180} />
+      ) : (
+        ''
+      )}
+      <h3>{fullname}</h3>
+      <p>
+        {summarytext.slice(0, 180)}
+        {summarytext.length > 180 && '...'}
+      </p>
     </li>
   );
 };
--- app/components/contentList/ContentList.tsx
+++ app/components/contentList/ContentList.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import ContentCard from '../contentCard/ContentCard';
+import ContentModal from '../contentModal/ContentModal';
 
 const ContentList = ({ content }) => {
+  // state for modal
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // function to open the modal
+  const openModal = (item) => {
+    setSelectedItem(item);
+    setIsModalOpen(true);
+  };
+
+  // function to close the modal
+  const closeModal = () => {
+    setSelectedItem(null);
+    setIsModalOpen(false);
+  };
   return (
-    <ul>
-      {content.map((item) => (
-        <ContentCard key={item.contentid} item={item} />
-      ))}
-    </ul>
+    <>
+      <ul>
+        {content.map((item) => (
+          <ContentCard
+            key={item.contentid}
+            item={item}
+            onClick={() => openModal(item)}
+          />
+        ))}
+      </ul>
+      {/* Modal */}
+      {isModalOpen && (
+        <ContentModal selectedItem={selectedItem} closeModal={closeModal} />
+      )}
+    </>
   );
 };
 
