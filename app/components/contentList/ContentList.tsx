746a68e23b482e11eaea00090679f1a3630c618a
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2025-06-12 10:38:03 +1000

    feat: create content list component to list the content

---
+++ app/components/contentList/ContentList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CategoryCard from '../contentCard/ContentCard';
+
+const ContentList = () => {
+  return (
+     <ul>
+           <CategoryCard/>
+     </ul>
+  )
+}
+
+export default ContentList
