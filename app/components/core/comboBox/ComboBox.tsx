e35dab0b292337e9e5013df0358e79e2c8d19ef2
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2025-06-17 09:46:10 +1000

    refactor: type the combobox component

--- app/components/core/comboBox/ComboBox.tsx
+++ app/components/core/comboBox/ComboBox.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
+import { FunctionComponent } from 'react';
 import { Autocomplete, TextField } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
-const StyledAutocomplete = styled(Autocomplete)(() => ({
+type ComboBoxProps = {
+  options: string[];
+  value: string;
+  onChangeValue: (value: string | null) => void;
+  label: string;
+};
+
+const StyledAutocomplete = styled(Autocomplete<string>)(() => ({
   backgroundColor: '#f9f9f9',
   borderRadius: 4,
 }));
 
-const ComboBox = ({ options = [], value, onChange, label }) => {
+const ComboBox: FunctionComponent<ComboBoxProps> = ({
+  options = [],
+  value = '',
+  onChangeValue,
+  label,
+}) => {
   return (
     <StyledAutocomplete
       fullWidth
       options={options}
-      value={value || null}
-      onChange={(event, newValue) => onChange(newValue || '')}
+      value={value}
+      onChange={(_event, newValue) => {
+        onChangeValue(newValue ?? '');
+      }}
       renderInput={(params) => (
         <TextField {...params} label={label} aria-label={label} />
       )}
