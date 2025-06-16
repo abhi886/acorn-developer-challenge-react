9ba0f8cba8ca46ec5afd5a6b86148321361ec20e
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2025-06-17 09:48:28 +1000

    refactor: rename the variable to onvaluechange

--- app/components/core/comboBox/ComboBox.tsx
+++ app/components/core/comboBox/ComboBox.tsx
@@ -6,7 +6,7 @@ import { styled } from '@mui/material/styles';
 type ComboBoxProps = {
   options: string[];
   value: string;
-  onChangeValue: (value: string | null) => void;
+  onValueChange: (value: string | null) => void;
   label: string;
 };
 
@@ -18,7 +18,7 @@ const StyledAutocomplete = styled(Autocomplete<string>)(() => ({
 const ComboBox: FunctionComponent<ComboBoxProps> = ({
   options = [],
   value = '',
-  onChangeValue,
+  onValueChange,
   label,
 }) => {
   return (
@@ -27,7 +27,7 @@ const ComboBox: FunctionComponent<ComboBoxProps> = ({
       options={options}
       value={value}
       onChange={(_event, newValue) => {
-        onChangeValue(newValue ?? '');
+        onValueChange(newValue ?? '');
       }}
       renderInput={(params) => (
         <TextField {...params} label={label} aria-label={label} />
