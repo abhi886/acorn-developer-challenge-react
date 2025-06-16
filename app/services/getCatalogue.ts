e0628b58200ecf5988b988f57d8db31fc7e5a718
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2025-06-16 11:14:20 +1000

    refactor: respond with the promise itself rather than the resolved value

--- app/services/getCatalogue.ts
+++ app/services/getCatalogue.ts
@@ -1,7 +1,5 @@
 import { fetchData } from '@/app/lib/utils/fetchData';
 
+const cataloguePath = '/local/acorn_coursemanagement/index.php/api/1.1/external_catalogue/188?perPage=16';
 
-export const getCatalogue = async () => {
-  const path = '/local/acorn_coursemanagement/index.php/api/1.1/external_catalogue/188?perPage=16';
-  return await fetchData(path);
-};
+export const getCatalogue = () => fetchData(cataloguePath);
