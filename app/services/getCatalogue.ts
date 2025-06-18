be7916c511438722414c768a364713bd66561e2b
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2025-06-18 13:04:45 +1000

    refactor: return promise from the function instead of the data

--- app/services/getCatalogue.ts
+++ app/services/getCatalogue.ts
@@ -2,4 +2,4 @@ import { fetchData } from '@/app/lib/utils/fetchData';
 
 const cataloguePath = '/local/acorn_coursemanagement/index.php/api/1.1/external_catalogue/188?perPage=16';
 
-export const getCatalogue = () => fetchData(cataloguePath);
+export const getCatalogue = <T>() => fetchData<T>(cataloguePath);
