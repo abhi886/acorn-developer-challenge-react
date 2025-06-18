1be1b819954582f585d11b09ce69b09573a13787
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2025-06-18 13:05:09 +1000

    feat: mock data to test the components

---
+++ app/data/catalogue.ts
@@ -0,0 +1,213 @@
+// data/catalogue.js or wherever you want to place it
+export const catalogue = {
+    status: "Complete",
+    data: {
+      items: [
+        {
+          fullname: "`Hello, World`! General site introductions.",
+          summary: "<p>Introduce yourself here!&nbsp;<br>📷 (Image credit to <a href=\"https://unsplash.com/@zgraves\">Zach Graves</a> on Unsplash)</p>",
+          summarytext: "Introduce yourself here! 📷 (Image credit to Zach Graves on Unsplash)",
+          contentid: 2043,
+          contenttype: "Course",
+          url: "https://staging.acornlms.com/view_course/2043",
+          imageurl: "https://staging.acornlms.com/local/acorn_shared/resources/assets/customfile.php/33610/course/overviewfiles/0/zach-graves-wtpTL_SzmhM-unsplash.jpg",
+          badge: "",
+          completionstatus: null,
+          programs: [],
+          category: {
+            id: 101,
+            name: "General Information"
+          },
+          tags: [
+            { id: 285, name: "Beginner Friendly" },
+            { id: 301, name: "Collaboration" },
+            { id: 303, name: "Communication" }
+          ],
+          customfields: [],
+          cost: 0,
+          duration: "",
+          timecreated: "2025-06-04T03:23:22+00:00",
+          timemodified: "2025-06-04T03:23:22+00:00",
+          contentstatus: "Active",
+          paymentCost: 0
+        },
+        {
+          fullname: "A ball of fluff with knives for hands - How to stop your cat...",
+          summary: "<p>Do you have a cat? Congratulations! I'm sure they're wonderful...</p>",
+          summarytext: "Do you have a cat Congratulations! ...",
+          contentid: 2044,
+          contenttype: "Course",
+          url: "https://staging.acornlms.com/view_course/2044",
+          imageurl: "https://staging.acornlms.com/local/acorn_shared/resources/assets/customfile.php/33612/course/overviewfiles/0/john-moeses-bauan-6mQLmJtLkEM-unsplash.jpg",
+          badge: "",
+          completionstatus: null,
+          programs: [],
+          category: {
+            id: 108,
+            name: "Work Life balance"
+          },
+          tags: [
+            { id: 315, name: "Cat pics" },
+            { id: 309, name: "Creativity" },
+            { id: 312, name: "Useful tools" },
+            { id: 287, name: "Advanced Concepts" }
+          ],
+          customfields: [],
+          cost: 0,
+          duration: "60h ",
+          timecreated: "2025-06-04T03:40:10+00:00",
+          timemodified: "2025-06-04T03:40:10+00:00",
+          contentstatus: "Active",
+          paymentCost: 0
+        },
+        {
+          fullname: "I'll lorem your ipsum: Creating dummy content that is still useful to work with.",
+          summary: "<p>When creating dummy markup for external use...</p>",
+          summarytext: "When creating dummy markup for external use...",
+          contentid: 117,
+          contenttype: "Resource",
+          url: "https://staging.acornlms.com/catalogue?tab=resources&collectionId=231757",
+          imageurl: "https://staging.acornlms.com/local/acorn_shared/resources/assets/customfile.php/1/content_resources_cover/content_resources_cover_117/0//cookie-the-pom-SNf-hZz6zOY-unsplash.jpg",
+          badge: "",
+          completionstatus: null,
+          programs: [],
+          category: {
+            id: 107,
+            name: "Byte-sized brilliance"
+          },
+          tags: [
+            { id: 295, name: "Code Samples" },
+            { id: 293, name: "Industry" },
+            { id: 304, name: "Problem solving" },
+            { id: 312, name: "Useful tools" },
+            { id: 302, name: "Time management" }
+          ],
+          customfields: [],
+          cost: 0,
+          duration: "",
+          timecreated: "2025-06-04T04:16:40+00:00",
+          timemodified: "2025-06-04T04:16:40+00:00",
+          contentstatus: "Active",
+          paymentCost: 0
+        },
+        {
+          fullname: "Open Floor IRL: How to network when you hate networking",
+          summary: "<p>Yes, the irony is not lost on us here...</p>",
+          summarytext: "Yes, the irony is not lost on us here...",
+          contentid: 1251,
+          contenttype: "Live Learning",
+          url: "https://staging.acornlms.com/view_facetoface/1251",
+          imageurl: "https://staging.acornlms.com/local/acorn_shared/resources/assets/customfile.php/1/facetoface_image/facetoface_image_1251/0//claudio-schwarz-xMCbZ6i4q6w-unsplash.jpg",
+          badge: "",
+          completionstatus: null,
+          programs: [],
+          category: {
+            id: 109,
+            name: "Open Floor Series"
+          },
+          tags: [
+            { id: 303, name: "Communication" },
+            { id: 301, name: "Collaboration" },
+            { id: 293, name: "Industry" },
+            { id: 286, name: "Intermediate Content" },
+            { id: 300, name: "Teamwork" }
+          ],
+          customfields: [],
+          cost: 5000,
+          duration: "2h 30m 25s",
+          timecreated: "2025-06-04T03:01:21+00:00",
+          timemodified: "2025-06-04T03:01:21+00:00",
+          contentstatus: "Active",
+          paymentCost: 0
+        },
+        {
+          fullname: "Open Floor: How to climb the corporate ladder without losing your soul",
+          summary: "<div><p>This <em>open floor</em> installment is designed...</p></div>",
+          summarytext: "This open floor installment is designed...",
+          contentid: 2041,
+          contenttype: "Course",
+          url: "https://staging.acornlms.com/view_course/2041",
+          imageurl: "https://staging.acornlms.com/local/acorn_shared/resources/assets/customfile.php/33608/course/overviewfiles/0/simon-woehrer-LT6iaXTGfMY-unsplash.jpg",
+          badge: "",
+          completionstatus: null,
+          programs: [],
+          category: {
+            id: 109,
+            name: "Open Floor Series"
+          },
+          tags: [
+            { id: 300, name: "Teamwork" },
+            { id: 302, name: "Time management" },
+            { id: 285, name: "Beginner Friendly" }
+          ],
+          customfields: [],
+          cost: 4000,
+          duration: "3h ",
+          timecreated: "2025-06-04T02:49:23+00:00",
+          timemodified: "2025-06-04T02:49:23+00:00",
+          contentstatus: "Active",
+          paymentCost: 0
+        },
+        {
+          fullname: "Open Floor: Useful tools to make your job easier.",
+          summary: "<p>Have any tools or hacks that help you out? Share it in this forum.</p>",
+          summarytext: "Have any tools or hacks that help you out Share it in this forum.",
+          contentid: 2042,
+          contenttype: "Course",
+          url: "https://staging.acornlms.com/view_course/2042",
+          imageurl: "",
+          badge: "",
+          completionstatus: null,
+          programs: [],
+          category: {
+            id: 109,
+            name: "Open Floor Series"
+          },
+          tags: [
+            { id: 285, name: "Beginner Friendly" },
+            { id: 308, name: "Best Practices" },
+            { id: 313, name: "Productivity" },
+            { id: 310, name: "Strategy" }
+          ],
+          customfields: [],
+          cost: 0,
+          duration: "15m ",
+          timecreated: "2025-06-04T03:15:24+00:00",
+          timemodified: "2025-06-04T03:15:24+00:00",
+          contentstatus: "Active",
+          paymentCost: 0
+        },
+        {
+          fullname: "The secret to maintaining morale while at work",
+          summary: "<p>(it's a video of a neat bird bopping around to 'it's not unusual' by Tom Jones.)</p>",
+          summarytext: "(it's a video of a neat bird bopping around to 'it's not unusual' by Tom Jones.)",
+          contentid: 71,
+          contenttype: "Video",
+          url: "https://staging.acornlms.com/catalogue?tab=videos&collectionId=231759",
+          imageurl: "https://staging.acornlms.com/local/acorn_shared/resources/assets/customfile.php/1/content_videos_cover/content_videos_cover_71/0//abdul-rehman-khalid-RtKCnWGRv_g-unsplash.jpg",
+          badge: "",
+          completionstatus: null,
+          programs: [],
+          category: {
+            id: 108,
+            name: "Work Life balance"
+          },
+          tags: [
+            { id: 312, name: "Useful tools" },
+            { id: 318, name: "Wellbeing" }
+          ],
+          customfields: [],
+          cost: 0,
+          duration: "",
+          timecreated: "2025-06-04T04:41:41+00:00",
+          timemodified: "2025-06-04T04:41:41+00:00",
+          contentstatus: "Active",
+          paymentCost: 0
+        }
+      ],
+      next_page_url: null,
+      previous_page_url: null,
+      per_page: 16
+    }
+  };
+  
