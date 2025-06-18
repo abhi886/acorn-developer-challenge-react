39587a9439a2079ef23cfe57d6e40979a5348b28
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2025-06-18 13:03:37 +1000

    refactor: type check the category search bar component

--- app/components/categorySearchBar/PropTypes.ts
+++ app/components/categorySearchBar/PropTypes.ts
@@ -1,14 +1,14 @@
 export type CategorySearchBarProps = {
-    searchTerm: string;
+     searchTerm: string | "";
      onSearchTermChange:(newTerm: string)=> void;
-     categoryOptions?: string[];
-     typeOptions?: string[];
-     tagOptions?: string[];
-     selectedCategory?: string;
-     selectedType?: string; 
-     selectedTags?: string[];
-     setSelectedCategory?: (category: string) => void;
-     setSelectedType?: (type: string) => void;
+     categoryOptions: string[];
+     typeOptions: string[];
+     tagOptions: string[];
+     selectedCategory: string;
+     selectedType: string; 
+     selectedTags: string[];
+     setSelectedCategory: (category: string) => void;
+     setSelectedType: (type: string) => void;
      setSelectedTags: React.Dispatch<React.SetStateAction<string[]>>;
      handleFilterClear: () => void;
      isFilterActive: boolean;
